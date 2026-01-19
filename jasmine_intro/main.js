c3a2ef2e68905c91378be58ea6502aa1157b972b
Elizbeh <105496336+Elizbeh@users.noreply.github.com>
2026-01-19 11:56:38 +0100

    Add fullName getter to User class

--- jasmine_intro/main.js
+++ jasmine_intro/main.js
@@ -3,43 +3,58 @@
 * Is there a more reliable way to identify our grouping name?
 *
 */
-
-class User {
-    firstName
-    lastName
-    middleName
-
-    constructor(data = {}) {
-        this.firstName = data.firstName || ""
-        this.lastName = data.lastName || ""
-        this.middleName = data.middleName || ""
-    }
-}
-
+import User from "./user.js";
 /**
 * Test Suite 
 */
 describe(`${User.name} Class`, () => {
+    let model;
+    beforeEach(()=> {
+        model = new User()
+    })
     describe("Default values", () => {
         it('first name defaults to empty', () => {
         //arrange
-    const data = {firstName: null}
-    //act
-    const model = new User(data)
-    //assert
-    expect(model.firstName).toBe('')
-    })
-   
-    it('last name defaults to empty', () => {
-        const data = {lastName: null}
-        const model = new User(data)
-        expect(model.lastName).toBe('')
-    })
-    it('middle name defaults to empty', () => {
-        const data = {middleName: null}
-        const model = new User(data)
-        expect(model.middleName).toBe('')
-    })
+            const data = {firstName: null}
+            //act
+        
+            //assert
+            expect(model.firstName).toBe('')
+            })
+        
+            it('last name defaults to empty', () => {
+                
+                expect(model.lastName).toBe('')
+            })
+            it('middle name defaults to empty', () => {
+        
+                expect(model.middleName).toBe('')
+            })
+         })
+
+    describe('Full name', () => {
+        beforeEach(() =>{
+            model = new User({
+                firstName: "Charle",
+                lastName: "Gabriel"
+            })
+            
+        })
+        it("middle initial when middle name is defined with first name and last name", () => {
+
+            model.middleName = "June"
+            const result = model.fullName
+            expect(result).toBe(`${model.firstName} ${model.middleName[0]}. ${model.lastName}`)
+
+        })
+        it('when no middle name return just first and last', ()=> {
+            model.middleName = ''
+            const result = model.fullName
+            expect(result).toBe(`${model.firstName} ${model.lastName}`)
+        })
     })
     
 })
+
+
+
--- jasmine_intro/user.js
+++ jasmine_intro/user.js
@@ -6,8 +6,15 @@ class User {
     constructor(data = {}) {
         this.firstName = data.firstName || ""
         this.lastName = data.lastName || ""
-        this.middleName = data.middleName 
+        this.middleName = data.middleName || ""
+    }
+
+    get fullName() {
+        if(this.middleName.length > 0){
+            return `${this.firstName} ${this.middleName[0]}. ${this.lastName}`
+        }
+        return `${this.firstName} ${this.lastName}`
     }
 }
 
-export default User;
+export default User
